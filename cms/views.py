f6f2d886e808296cb76a7d170496ecac40c5e3e9
Jesús <jgalanb>
2016-03-30 19:19:14 +0200

    Modificado fichero views.py por Jesus Galan Barba

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
-# Create your views here.
+def id_to_page(request, identificador):
+    try:
+        elementos = Pages.objects.get(id=int(identificador))
+        http_Resp = elementos.page
+    except Pages.DoesNotExist:
+        http_Resp = "<h3><font color='red'>Error! El indice introducido no " +\
+                    "corresponde con ningun elemento de la tabla!</font></h3>"
+
+    return HttpResponse(http_Resp)
+
+def name_to_page(request, recurso):
+    try:
+        elementos = Pages.objects.get(name=recurso)
+        http_Resp = elementos.page
+    except Pages.DoesNotExist:
+        http_Resp = "<h3><font color='red'>Error! El recurso introducido no " +\
+                    "corresponde con ningun elemento de la tabla!</font></h3>"
+
+    return HttpResponse(http_Resp)
